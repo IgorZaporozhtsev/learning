2a49a920c9e04c03c444d77e67fe88abd1533d95
zeecoder <igorzaporogtsev@gmail.com>
2020-09-26 10:18:30 +0300

    add builder3 design pattern

---
+++ src/design_patterns/builder/Builder_DesignPattern_3.java
@@ -0,0 +1,90 @@
+package design_patterns.builder;
+
+public class Builder_DesignPattern_3 {
+    public static void main(String[] args) {
+        PizzaBuilder pizzaBuilder = new HawaiianPizzaBuilder();
+        pizzaBuilder.createNewPizzaProduct();
+
+        pizzaBuilder.buildDough();
+        pizzaBuilder.buildSauce();
+        pizzaBuilder.buildTopping();
+
+        Pizza pizza = pizzaBuilder.getPizza();
+
+        System.out.println(pizza);
+    }
+}
+
+abstract class PizzaBuilder {
+    protected Pizza pizza;
+
+    public Pizza getPizza() {
+        return pizza;
+    }
+
+    public void createNewPizzaProduct() {
+        pizza = new Pizza();
+    }
+
+    public abstract void buildDough();
+
+    public abstract void buildSauce();
+
+    public abstract void buildTopping();
+}
+
+class HawaiianPizzaBuilder extends PizzaBuilder {
+    public void buildDough() {
+        pizza.setDough("cross");
+    }
+
+    public void buildSauce() {
+        pizza.setSauce("mild");
+    }
+
+    public void buildTopping() {
+        pizza.setTopping("ham+pineapple");
+    }
+}
+
+class SpicyPizzaBuilder extends PizzaBuilder {
+    public void buildDough() {
+        pizza.setDough("pan baked");
+    }
+
+    public void buildSauce() {
+        pizza.setSauce("hot");
+    }
+
+    public void buildTopping() {
+        pizza.setTopping("pepperoni+salami");
+    }
+}
+
+class Pizza {
+    private String dough = "";
+    private String sauce = "";
+    private String topping = "";
+
+    public void setDough(String dough) {
+        this.dough = dough;
+    }
+
+    public void setSauce(String sauce) {
+        this.sauce = sauce;
+    }
+
+    public void setTopping(String topping) {
+        this.topping = topping;
+    }
+
+    @Override
+    public String toString() {
+        return "Pizza{" +
+            "dough='" + dough + '\'' +
+            ", sauce='" + sauce + '\'' +
+            ", topping='" + topping + '\'' +
+            '}';
+    }
+}
+
