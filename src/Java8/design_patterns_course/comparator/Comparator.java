e05d3089d8d44d26cb9eab244c32d889570f7b95
zeecoder <igorzaporogtsev@gmail.com>
2020-11-23 09:42:29 +0200

    refactoring with Comparator

--- src/Java8/design_patterns_course/comparator/Comparator.java
+++ src/Java8/design_patterns_course/comparator/Comparator.java
@@ -6,15 +6,14 @@ import java.util.Objects;
 
 @FunctionalInterface
 public interface Comparator<T> {
+
     int compare(T t1, T t2);
 
     static Comparator<Person> comparing(Function<Person, String> keyExtractor) {
         Objects.requireNonNull(keyExtractor);
 
-        return (p1, p2) -> {
-            String s1 = keyExtractor.apply(p1);
-            String s2 = keyExtractor.apply(p2);
-            return s1.compareTo(s2);
-        };
+        Comparator<Person> comparator = (t1, t2) -> keyExtractor.apply(t1).compareTo(keyExtractor.apply(t2));
+
+        return comparator;
     }
 }
--- src/Java8/design_patterns_course/comparator/PlayWithComparators.java
+++ src/Java8/design_patterns_course/comparator/PlayWithComparators.java
@@ -1,5 +1,8 @@
 package Java8.design_patterns_course.comparator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayWithComparators {
     public static void main(String[] args) {
         Person mary = new Person("Mary", 28);
@@ -7,15 +10,32 @@ public class PlayWithComparators {
         Person linda = new Person("Linda", 26);
         Person james = new Person("James", 32);
 
-        Comparator<Person> cmpName = Comparator.comparing(Person::getName);
+        List<Person> people = new ArrayList<>();
+        people.add(mary);
+        people.add(john);
+        people.add(linda);
+        people.add(james);
 
-        boolean isMaryEqJohn = cmpName.compare(mary, john) > 0;
-        boolean isJohnEqJames = cmpName.compare(john, james) > 0;
-        boolean isLindaEqJohn = cmpName.compare(linda, john) > 0;
+        //people.sort((Person p1, Person p2)-> p1.getName().compareTo(p2.getName()));
+        //people.sort(java.util.Comparator.comparing(Person::getName));
 
-        System.out.println("Mary > John : " + isMaryEqJohn);
-        System.out.println("John > James : " + isJohnEqJames);
-        System.out.println("Linda > John : " + isLindaEqJohn);
+
+        Comparator<Person> cmpByName = Comparator.comparing(Person::getName);
+        //Comparator<Person> cmpByName = (p1, p2) -> p1.getAge() - p2.getAge();
+        //Comparator<Person> cmpByName = (p1, p2) -> p1.getName().compareTo(p2.getName());
+
+
+        int result1 = cmpByName.compare(mary, john);
+        boolean isMaryEqJohn = result1 > 0;
+        System.out.println("compare result: " + result1 + " then Mary > John: " + isMaryEqJohn);
+
+        int result2 = cmpByName.compare(john, james);
+        boolean isJohnEqJames = result2 > 0;
+        System.out.println("compare result: " + result2 + " then John > James : " + isJohnEqJames);
+
+        int result3 = cmpByName.compare(linda, john);
+        boolean isLindaEqJohn = result3 > 0;
+        System.out.println("compare result: " + result3 + " then Linda > John : " + isLindaEqJohn);
     }
 
 
