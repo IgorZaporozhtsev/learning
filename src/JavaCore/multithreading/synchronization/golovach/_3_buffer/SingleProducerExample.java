e4fe7db059cf007c7821952f66271730878ac0d3
zeecoder <igorzaporogtsev@gmail.com>
2021-01-18 19:14:38 +0200

    delete & refactor Producer Example

--- src/JavaCore/multithreading/synchronization/golovach/_3_buffer/SingleProducerExample.java
+++ src/JavaCore/multithreading/synchronization/golovach/_3_buffer/SingleProducerExample.java
@@ -9,3 +9,75 @@ public class SingleProducerExample {
   }
 
 }
+
+class Producer implements Runnable {
+  private final int period;
+  private final SingleElementBuffer buffer;
+  private int startValue;
+
+  public Producer(int startValue, int period,
+                  SingleElementBuffer buffer) {
+    this.startValue = startValue;
+    this.period = period;
+    this.buffer = buffer;
+  }
+
+  @Override
+  public void run() {
+    while (true) {
+      try {
+        System.out.println(startValue + " produced");
+        buffer.put(startValue++);
+        Thread.sleep(period);
+      } catch (InterruptedException e) {
+        System.out.println(Thread.currentThread().getName());
+        return;
+      }
+    }
+  }
+}
+
+
+class Consumer implements Runnable {
+  private final SingleElementBuffer buffer;
+
+  public Consumer(SingleElementBuffer buffer) {
+    this.buffer = buffer;
+  }
+
+  @Override
+  public void run() {
+    while (true) {
+      try {
+        int elem = buffer.get();
+        System.out.println(elem + " consumed");
+      } catch (InterruptedException e) {
+        System.out.println(Thread.currentThread().getName());
+        return;
+      }
+    }
+  }
+}
+
+class SingleElementBuffer {
+  private Integer elem = null;
+
+  public synchronized void put(Integer newElem) throws InterruptedException {
+    while (elem != null) {
+      this.wait();
+    }
+    this.elem = newElem;
+    this.notifyAll();
+  }
+
+  public synchronized Integer get() throws InterruptedException {
+    while (elem == null) {
+      this.wait();
+    }
+    int result = this.elem;
+    this.elem = null;
+    this.notifyAll();
+    return result;
+  }
+
+}
