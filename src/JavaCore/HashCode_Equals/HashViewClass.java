7d24b641014872637ad1e39e686e5f2782194cf4
zeecoder <igorzaporogtsev@gmail.com>
2021-01-23 10:39:41 +0200

    work with hashcode and equals

--- src/JavaCore/HashCode_Equals/HashViewClass.java
+++ src/JavaCore/HashCode_Equals/HashViewClass.java
@@ -1,11 +1,14 @@
 package JavaCore.HashCode_Equals;
 
+import java.util.Objects;
+
 public class HashViewClass {
     public static void main(String[] args) {
         BlackBox blackBox = new BlackBox(15, 123);
         int hCode;
         hCode = blackBox.hashCode();
         System.out.println(hCode);
+        System.out.println(Objects.hashCode(634));
     }
 
     public static class BlackBox{
