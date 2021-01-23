4e5f99e615be04918e0b6cfd6506dda584ac3906
zeecoder <igorzaporogtsev@gmail.com>
2021-01-23 10:36:41 +0200

    ofNullable Optional

---
+++ src/Java8/Optional/MainOptional.java
@@ -0,0 +1,12 @@
+package Java8.Optional;
+
+import java.util.Optional;
+
+public class MainOptional {
+    public static void main(String[] args) {
+        Optional.of("");
+        Optional<String> s = Optional.ofNullable("");
+
+
+    }
+}
