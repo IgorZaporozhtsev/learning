c36b4839605434142cbf9f4d5c205cd17ddd34da
zeecoder <igorzaporogtsev@gmail.com>
2021-01-23 10:38:45 +0200

    reafactor Function Example

--- src/Java8/functional_interface/function/FunctionExample_1.java
+++ src/Java8/functional_interface/function/FunctionExample_1.java
@@ -1,6 +1,6 @@
 package Java8.functional_interface.function;
 
-import Java8.prepared_data.HobbyStream;
+import Java8.prepared_data.Hobby;
 import Java8.prepared_data.HobbyType;
 import Java8.prepared_data.PersonStream;
 
@@ -25,27 +25,27 @@ public class FunctionExample_1 {
 
 class Foo {
 
-    public List<? extends HobbyStream> fooHobbies(List<PersonStream> personStreams) {
-        Function<PersonStream, HobbyStream> fooMethod = personStream -> fooMethod(personStream);
+    public List<? extends Hobby> fooHobbies(List<PersonStream> personStreams) {
+        Function<PersonStream, Hobby> fooMethod = personStream -> fooMethod(personStream);
         return build(personStreams, fooMethod);
     }
 
-    public List<? extends HobbyStream> hooHobbies(List<PersonStream> personStreams) {
+    public List<? extends Hobby> hooHobbies(List<PersonStream> personStreams) {
         return build(personStreams, this::hooMethod);
     }
 
 
-    public List<? extends HobbyStream> build(List<PersonStream> persons, Function<PersonStream, HobbyStream> function) {
+    public List<? extends Hobby> build(List<PersonStream> persons, Function<PersonStream, Hobby> function) {
         return persons.stream()
             .map(function)
             .collect(Collectors.toList());
     }
 
-    public HobbyStream fooMethod(PersonStream personStream) {
-        return new HobbyStream("chess", 7, HobbyType.INTELLIGENCE);
+    public Hobby fooMethod(PersonStream personStream) {
+        return new Hobby("chess", 7, HobbyType.INTELLIGENCE);
     }
 
-    public HobbyStream hooMethod(PersonStream personStream) {
-        return new HobbyStream("chess", 7, HobbyType.INTELLIGENCE);
+    public Hobby hooMethod(PersonStream personStream) {
+        return new Hobby("chess", 7, HobbyType.INTELLIGENCE);
     }
 }
