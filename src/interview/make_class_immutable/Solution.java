d20403b3dd376fca8039d956dfab2c701cb60d2c
ihor <i.zaporozhtsev@gmail.com>
2023-05-11 17:20:25 +0300

    add code

---
+++ src/interview/make_class_immutable/Solution.java
@@ -0,0 +1,69 @@
+package interview.make_class_immutable;
+
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+
+class Solution {
+
+    public static void main(String[] args) {
+
+        Set<Reservation> reservations = Set.of(
+                new Reservation(1),
+                new Reservation(2),
+                new Reservation(3),
+                new Reservation(4),
+                new Reservation(5),
+                new Reservation(6)
+        );
+        System.out.println(reservations.contains(new Reservation(1)));
+    }
+
+
+    public static class Reservation {
+        public int id;
+        public String clientFullName;
+        public List<LocalDate> reservationDates;
+
+
+        public Reservation(Integer id) {
+            this.id = id;
+            if (id < 0) {
+                throw new IllegalArgumentException("id cannot be negative");
+            }
+        }
+
+        public Reservation(Integer id, String clientFullName) {
+            this.id = id;
+            this.clientFullName = clientFullName;
+
+            if (id < 0) {
+                throw new IllegalArgumentException("id cannot be negative");
+            }
+        }
+
+        public Reservation(Integer id, String clientFullName, List<LocalDate> reservationDates) {
+            this.id = id;
+            this.clientFullName = clientFullName;
+            this.reservationDates = reservationDates;
+            if (id < 0) {
+                throw new IllegalArgumentException("id cannot be negative");
+            }
+        }
+
+        public Integer getId() {
+            return id;
+
+        }
+
+        public String getClientFullName() {
+            return clientFullName;
+        }
+
+        public List<LocalDate> getReservationDates() {
+            return reservationDates;
+        }
+
+    }
+}
