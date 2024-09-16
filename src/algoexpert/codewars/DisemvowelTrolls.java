970792ae3b058ff7f9958c48cba6f31fd244d6bf
ihor <i.zaporozhtsev@gmail.com>
2024-09-16 10:42:26 +0300

    dally challenge

--- src/algoexpert/codewars/DisemvowelTrolls.java
+++ src/algoexpert/codewars/DisemvowelTrolls.java
@@ -1,14 +1,30 @@
 package algoexpert.codewars;
 
 import java.util.Arrays;
+import java.util.function.IntPredicate;
+import java.util.stream.Collectors;
 
 public class DisemvowelTrolls {
 
     public static void main(String[] args) {
-        String disemvowel = disemvowel("This website is for losers LOL!");
+        String disemvowel = disemvowel2("This website is for losers LOL!");
         System.out.println(disemvowel);
     }
     public static String disemvowel(String str) {
         return str.replaceAll("[aeiouAEIOU]", "");
+    }
+
+    public static String disemvowel2(String str) {
+        IntPredicate notVowelPredicate = character ->
+                character != 'A' && character != 'a'
+                        && character != 'E' && character != 'e'
+                        && character != 'I' && character != 'i'
+                        && character != 'O' && character != 'o'
+                        && character != 'U' && character != 'u';
+
+        return str.chars()
+                .filter(notVowelPredicate)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining());
     }
 }
