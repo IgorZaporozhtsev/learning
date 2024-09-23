bfbae483e5765a9cc3b3ba0fdde108ee9c5dcc65
ihor <i.zaporozhtsev@gmail.com>
2024-09-23 08:26:06 +0300

    dally challenge

--- src/algoexpert/codewars/ListFiltering.java
+++ src/algoexpert/codewars/ListFiltering.java
@@ -1,7 +1,10 @@
 package algoexpert.codewars;
 
+import Java8.functional_interface.predicate.PredicateTest;
+
 import java.io.Serializable;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class ListFiltering {
     //https://www.codewars.com/kata/53dbd5315a3c69eed20002dd/train/java
@@ -13,7 +16,7 @@ public class ListFiltering {
     public static List<Object> filterList(final List<Object> list) {
         // Return the List with the Strings filtered out
         return list.stream()
-                .filter(item -> !(item instanceof String))
+                .filter(Predicate.not(item-> item instanceof String))
                 .toList();
     }
 }
