f4ff83057730c19fbdd7355cca5f5c5e627eaa25
ihor <i.zaporozhtsev@gmail.com>
2024-09-23 09:36:55 +0300

    dally challenge

--- src/algoexpert/leetcode/slide_window/BestTimetoBuyandSellStock_121.java
+++ src/algoexpert/leetcode/slide_window/BestTimetoBuyandSellStock_121.java
@@ -6,26 +6,28 @@ public class BestTimetoBuyandSellStock_121 {
     //121. Best Time to Buy and Sell Stock
     public static void main(String[] args) {
         int[] arr1 = {2,1,2,1,0,1,2};
-        int res1 = maxProfit(arr1);
+        int[] arr2 = {7,1,5,3,6,4};
+        int res1 = maxProfit(arr2);
         System.out.println(res1);
     }
 
     public static int maxProfit(int[] prices) {
 
-        int l = 0;
-        int profit = 0;
-
-        for (int r = 1; r < prices.length; r++) {
-            if (prices[l] > prices[r]){
-                  l = r;
+        int left = 0;
+        int right = 1;
+        int maxProfit = 0;
+        while (prices.length > right){
+            if (prices[right] < prices[left]){
+                left=right;
             } else {
-                int diff = prices[r] - prices[l];
-                if (diff > profit){
-                    profit = diff;
+                int profit = prices[right] - prices[left];
+                right++;
+                if (profit > maxProfit){
+                    maxProfit = profit;
                 }
             }
         }
-        return profit;
+        return maxProfit;
     }
 
 
