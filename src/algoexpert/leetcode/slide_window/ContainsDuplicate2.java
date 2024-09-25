2c5b9d69feab2c2c04d36a9262c4954d00b2552d
ihor <i.zaporozhtsev@gmail.com>
2024-09-25 09:12:05 +0300

    dally challenge

--- src/algoexpert/leetcode/slide_window/ContainsDuplicate2.java
+++ src/algoexpert/leetcode/slide_window/ContainsDuplicate2.java
@@ -9,16 +9,17 @@ public class ContainsDuplicate2 {
         int[] nums1 = {1,2,3,1};
         int[] nums2 = {1,0,0,1};
         int[] nums3 = {1,2,3,1,2,3};
-        System.out.println(containsNearbyDuplicate(nums1, 3));
-        System.out.println(containsNearbyDuplicate(nums2, 1));
-        System.out.println(containsNearbyDuplicate(nums3, 2));
+        int[] nums4 = {2,1,3,1};
+        //System.out.println(containsNearbyDuplicate(nums1, 3));
+        //System.out.println(containsNearbyDuplicate(nums2, 1));
+        //System.out.println(containsNearbyDuplicate(nums3, 2));
+        System.out.println(containsNearbyDuplicate(nums4, 2));
     }
 
     private static boolean containsNearbyDuplicate(int[] nums, int k) {
         HashSet<Integer> window = new HashSet<>();
-        int left = 0;
 
-        for (int right = 0; right < nums.length; right++) {
+        for (int right = 0, left = 0; right < nums.length; right++) {
             if (right - left > k){
                 window.remove(nums[left]);
                 left++;
