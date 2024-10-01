aff230818527a51c30c7a743d29d0b9e466faf62
ihor <i.zaporozhtsev@gmail.com>
2024-10-01 12:26:33 +0300

    dally challenge

--- src/algoexpert/leetcode/arrays/binary_search/SearchInsertPosition.java
+++ src/algoexpert/leetcode/arrays/binary_search/SearchInsertPosition.java
@@ -6,45 +6,32 @@ import java.util.Map;
 public class SearchInsertPosition {
     //35. Search Insert Position
 
-    /*
-     Input: nums = [1,3,5,6], target = 5
-     Output: 2
-     Input: nums = [1,3,5,6], target = 2
-     Output: 1
-     Input: nums = [1,3,5,6], target = 7
-     Output: 4
+    public static void main(String[] args) {
+        int[] nums1 = {1,3,5,6}; int target1 = 5;
+        int[] nums2 = {1,3,5,6}; int target2 = 2;
+        int[] nums3 = {1,3,5,6}; int target3 = 7;
 
 
-    * */
+        //System.out.println(searchInsert(nums1, target1));
+        //System.out.println(searchInsert(nums2, target2));
+        System.out.println(searchInsert(nums3, target3));
+    }
 
 
     public static int searchInsert(int[] nums, int target) {
-        System.out.println("target is "+target);
-        int green = 0;
-        int blue = nums.length - 1;
-
-        while(green <= blue){
-            int red = green + (blue - green)/2;
-            if(target > nums[red]){
-                green = red + 1;
-            }else{
-                blue = red - 1;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right){
+            int middle = (left + right) / 2;
+            if (nums[middle] == target){
+                return middle;
+            }
+            if (target > nums[middle] ){
+                left = middle + 1;
+            } else {
+                right = middle - 1;
             }
         }
-        return green;
+        return left;
     }
-
-    public static void main(String[] args) {
-        var integerMap = Map.of(
-//                new int[]{1, 3}, 2
-//                  new int[]{1, 3, 5, 6}, 0
-                new int[]{1, 3, 5, 6}, 5
-//                new int[]{1, 3, 5, 6}, 2
-//                new int[]{1, 3, 5, 6}, 7
-        );
-
-        integerMap.forEach((nums, target) -> System.out.println
-                ("Input: "+ Arrays.toString(nums) +" Output index is " + searchInsert(nums, target)));
-    }
-
 }
