b9e8b7253bc9a811af637562605692aad102817f
ihor <i.zaporozhtsev@gmail.com>
2024-10-17 10:09:42 +0300

    dally challenge

---
+++ src/algoexpert/codewars/ThinkingAndTesting.java
@@ -0,0 +1,12 @@
+package algoexpert.codewars;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ThinkingAndTesting {
+
+
+
+    public static int testTrueFalse(int n) {
+        return Integer.bitCount(n);
+    }
+}
