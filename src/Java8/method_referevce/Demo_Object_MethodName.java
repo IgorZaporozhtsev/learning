04b5ee7aaa1840ec407b6d5ccfcdc8ac66ac11ab
Ihor Zaporozhtsev <i.zaporozhtsev@gmail.com>
2026-04-09 11:07:40 +0300

    MethodReference

--- src/Java8/method_referevce/Demo_Object_MethodName.java
+++ src/Java8/method_referevce/Demo_Object_MethodName.java
@@ -2,14 +2,25 @@ package Java8.method_referevce;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class Demo_Object_MethodName {
 
 	public static void main(String[] args) {
 		User currentUser = new User(19);
 		AccessControlService service = new AccessControlService();
+		//2.Object::instanceMethodName
 		List<Resource> availableResources = service.getAvailableResources(currentUser, List.of());
 
+		//2 and 3 together
+		AccessControlService service1= new AccessControlService();
+		Supplier<String> statusSupplier = service1::changeStatus;
+		Function<AccessControlService, String> statusFunction = AccessControlService::changeStatus;
+
+		String hello = "hello";
+		Supplier<String> stringSupplier = hello::toUpperCase;
+		Function<String, String> stringFunction = String::toUpperCase;
 	}
 }
 
@@ -28,6 +39,12 @@ class AccessControlService {
 			.filter(manager::isOlderThan)      // відносно конкретного User manager
 			.sorted(Comparator.comparingInt(manager::getMiddleAge))
 			.toList();
+	}
+
+
+
+	public String changeStatus(){
+		return "Status changed".toUpperCase();
 	}
 }
 
